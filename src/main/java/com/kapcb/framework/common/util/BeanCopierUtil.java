4cce734961de5895657210ee219d6bbaecadee9d
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-11-23 08:35:00 +0800

    bean copier

--- src/main/java/com/kapcb/framework/common/util/BeanCopierUtil.java
+++ src/main/java/com/kapcb/framework/common/util/BeanCopierUtil.java
@@ -1,6 +1,10 @@
 package com.kapcb.framework.common.util;
 
+import cn.hutool.core.lang.Assert;
 import lombok.experimental.UtilityClass;
+import org.springframework.cglib.beans.BeanCopier;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * <a>Title: BeanCopierUtil </a>
@@ -13,4 +17,24 @@ import lombok.experimental.UtilityClass;
  */
 @UtilityClass
 public class BeanCopierUtil {
+
+    private static final ConcurrentHashMap<String, BeanCopier> BEAN_COPIER_MAP = new ConcurrentHashMap<>(6);
+
+    public static void copy(Object source, Object target) {
+        Assert.notNull(source, "source can not be null");
+        Assert.notNull(target, "target can not be null");
+        String key = genKey(source.getClass(), target.getClass());
+        BeanCopier beanCopier;
+        if (BEAN_COPIER_MAP.containsKey(key)) {
+            beanCopier = BEAN_COPIER_MAP.get(key);
+        } else {
+            beanCopier = BeanCopier.create(source.getClass(), target.getClass(), false);
+            BEAN_COPIER_MAP.putIfAbsent(key, beanCopier);c
+        }
+        beanCopier.copy(source, target, null);
+    }
+
+    private static String genKey(Class<?> sourceClazz, Class<?> targetClazz) {
+        return new String();
+    }
 }
